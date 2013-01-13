67e7c54be4f36fae17ef2a1374d2ae3ac9e0293d
Filippo Valsorda <filippo.valsorda@gmail.com>
2013-01-13 16:10:37 +0100

    a simple Heroku + MongoDB api for storing bad words

---
+++ app.py
@@ -0,0 +1,29 @@
+import web
+import os
+import urllib2
+from pymongo import MongoClient
+import datetime
+
+DEBUG = (os.environ.get('DEBUG') == '1')
+connection = MongoClient(os.environ['MONGOLAB_URI'])
+db = connection.heroku_app10932334
+
+urls = (
+    '/bad', 'bad',
+)
+
+class bad:
+    def GET(self):
+        word = web.input().get('word')
+        if not word:
+            raise web.forbidden()
+
+        bad_word = { 'word': word,
+                     'ip': web.ctx.ip,
+                     'time': datetime.datetime.utcnow() }
+        return db.bad.insert(bad_word)
+
+
+application = web.application(urls, globals())
+if DEBUG: application.internalerror = web.debugerror
+app = application.wsgifunc()
